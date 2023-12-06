8e72fd74a343a094d685e8037952d788f927d938
Aashish <aashishnkumar@gmail.com>
2023-12-06 20:30:57 +0530

    Removed redundant files

--- alert_dispatchers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlertDispatchersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alert_dispatchers'
--- data_harvesters/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataHarvestersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'data_harvesters'
